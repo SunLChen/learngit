df98a372daf1a5fd6e0609fef8760e6e12965540
SunLChen <1987649462@qq.com>
2018-04-12 18:00:35 -0700

    code 1

---
+++ hello.cc
@@ -0,0 +1,13 @@
+ ///
+ /// @file    hello.cc
+ /// @author  cgy(cgy@qq.com)
+ /// @date    2018-04-12 17:59:13
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+int main (){
+	cout<<"hello,world"<<endl;
+	return 0;
+}
